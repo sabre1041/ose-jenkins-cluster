c885a2045ba705c66da43ec00dd14d3881742d4a
Andrew Block <andy.block@gmail.com>
2016-01-03 21:57:51 -0600

    Parameter cleanup in slave script

--- jenkins-slave/jenkins-slave.sh
+++ jenkins-slave/jenkins-slave.sh
@@ -10,7 +10,6 @@ if [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
   JAR=`ls -1 /opt/jenkins-slave/bin/swarm-client-*.jar | tail -n 1`
 
   if [[ "$@" != *"-master "* ]] && [ ! -z "$JENKINS_PORT_8080_TCP_ADDR" ]; then
-    PARAMS="-master http://$JENKINS_SLAVE_SERVICE_HOST:$JENKINS_PORT_8080_TCP_PORT"
 	PARAMS="-master http://$JENKINS_SERVICE_HOST:$JENKINS_SERVICE_PORT -tunnel $JENKINS_SLAVE_SERVICE_HOST:$JENKINS_SLAVE_SERVICE_PORT -username ${master_username} -password ${master_password} -executors ${slave_executors}"
   fi
 
