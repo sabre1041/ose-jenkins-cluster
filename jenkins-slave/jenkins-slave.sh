545039c7a92b4319cc2c7d2885630890424a7d8d
Andrew Block <andy.block@gmail.com>
2016-02-03 08:15:22 -0500

    Adding context path support to jenkins slaves

--- jenkins-slave/jenkins-slave.sh
+++ jenkins-slave/jenkins-slave.sh
@@ -33,7 +33,7 @@ elif [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
   JAR=`ls -1 /opt/jenkins-slave/bin/swarm-client-*.jar | tail -n 1`
 
   if [[ "$@" != *"-master "* ]] && [ ! -z "$JENKINS_PORT_8080_TCP_ADDR" ]; then
-	PARAMS="-master http://$JENKINS_SERVICE_HOST:$JENKINS_SERVICE_PORT -tunnel $JENKINS_SLAVE_SERVICE_HOST:$JENKINS_SLAVE_SERVICE_PORT -username ${master_username} -password ${master_password} -executors ${slave_executors}"
+	PARAMS="-master http://${JENKINS_SERVICE_HOST}:${JENKINS_SERVICE_PORT}${JENKINS_CONTEXT_PATH} -tunnel ${JENKINS_SLAVE_SERVICE_HOST}:${JENKINS_SLAVE_SERVICE_PORT}${JENKINS_SLAVE_CONTEXT_PATH} -username ${master_username} -password ${master_password} -executors ${slave_executors}"
   fi
 
   echo Running java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
